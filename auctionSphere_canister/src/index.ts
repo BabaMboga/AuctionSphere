0a330e45a0b7bcc2714d84c2d0f441549e874ddf
Ayim William <odhisayim@gmail.com>
2023-11-28 19:26:40 +0300

    Cleared functions and started with new global structures

--- auctionSphere_canister/src/index.ts
+++ auctionSphere_canister/src/index.ts
@@ -1,86 +1,16 @@
-import {Canister, query, text, update, Principal, Vec, Opt, None, float64, Void} from 'azle'
+import {Canister, query, update, int64, text, Principal, bool, Record} from 'azle'
 
-type Bid = {
-    bidder: Principal;
-    amount: float64;
-    item: text;
-}
-//Global variables to store auction state
+// Define global auction item structure
 
-let currentBid: Opt<Bid> = None;
-let auctionEndTime: float64 = 0;
-let auctionHistory: Vec<Bid> | null = null;
+const auctionItem = Record({
+    seller: Principal,
+    startTime: int64,
+    endTime: int64,
+    currentBid: int64,
+    highestBidder: Principal,
+    canceled: bool,
+});
 
-export default Canister({
-    // Query functions to retrieve auction state
+// create a map to store auction items
+let auctions = new Map<text, typeof auctionItem>();
 
-    // function to get the current bid made
-    getCurrentBid: query([], text, () => {
-        if (currentBid) {
-            return JSON.stringify(currentBid);
-        
-        } else {
-            return '';
-        }
-    }),
-
-    // function to get the auction end time
-    getAuctionEndTime: query([], text, () => {
-        return auctionEndTime.toString();
-    }),
-
-    // function to get the auction history
-    getAuctionHistory: query([], text, () => {
-        try {
-            return JSON.stringify(auctionHistory);
-        } catch (error) {
-            console.error('Error getting auction history:', error);
-            return '';
-        }
-    }),
-
-    // Update functions to modify auction state
-    placeBid: update([text, float64], Void, (bidder, amount) => {
-        // Validate bid and update auction state if valid
-        if (validateBid(bidder, amount)) {
-            currentBid = {bidder, amount};
-            if (!auctionHistory) {
-                auctionHistory = new Vec<Bid>;
-            }
-            auctionHistory?.push(currentBid);
-        }
-    }),
-
-    endAuction: update([], Void, () => {
-        //end the auction and perform any necessary actions
-    }),
-
-    cancelAuction: update([], Void, () => {
-        //Cancel the auction if hasnt ended yet
-        if (block_timestamp() < auctionEndTime) {
-            currentBid = null;
-            auctionEndTime = 0;
-        }
-    }),
-
-    makeBid: update([text, float64], Void, (bidder, amount) => {
-        //place a bid and keep updating until it becomes the highest bid
-        placeBid(bidder, amount);
-
-        while (block_timestamp() < auctionEndTime) {
-            const currentBid = getCurrentBid();
-            if (!current || currentBid.amount < amount) {
-                placeBid(bidder, amount);
-            } else {
-                break;
-            }
-        }
-    }),
-
-    extendAuction: update([float64], Void, (newEndTime) => {
-        // Extend the auction if it hasn't ended yet
-        if (block_timestamp() < auctionEndTime) {
-            auctionEndTime = newEndTime;
-        }
-    }),
-})
